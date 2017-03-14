e1b243692db1d187375403b0c4b724f5e4b6d4d3
kayesspea <kperk14@gmail.com>
2017-03-13 20:53:35 -0500

    Added Resources and Changed Snippets
    - Added Resource List
    - Changed Snippet Format
    - Updated Styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $('.solution').hide();
+  $('.art-code-snippet'),hide();
 
   $('.toggle-one').click(function() {
     $('.toggle-one').text(function(_, text) {
@@ -17,6 +18,13 @@ $(document).ready(function() {
 
   $('.toggle-three').click(function() {
     $('.toggle-three').text(function(_, text) {
+      return text === 'Show Solution' ? "Hide Solution" : "Show Solution"
+    })
+    $('.solution-three').toggle(400);
+  });
+
+  $('.red-ninja-toggle').click(function() {
+    $('.red-ninja-toggle').text(function(_, text) {
       return text === 'Show Solution' ? "Hide Solution" : "Show Solution"
     })
     $('.solution-three').toggle(400);
